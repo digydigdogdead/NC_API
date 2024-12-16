b7337fe216395c41a749bb59c4514203329df33e
digydigdogdead <carlyle.laurent@gmail.com>
2024-12-16 15:10:10 +0100

    Add book and response classes

---
+++ API/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace API
+{
+    internal class Book
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; } = 0;
+        [JsonPropertyName("firstname")]
+        public string FirstName { get; set; } = "";
+        [JsonPropertyName("lastname")]
+        public string LastName { get; set; } = "";
+        [JsonPropertyName("email")]
+        public string Email { get; set; } = "";
+        [JsonPropertyName("phone")]
+        public string PhoneNumber { get; set; } = "";
+        [JsonPropertyName("birthday")]
+        public DateTime Birthday { get; set; }
+    }
+}
---
+++ API/Response.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace API
+{
+    internal class Response
+    {
+        [JsonPropertyName("status")]
+        public string Status {  get; set; }
+        [JsonPropertyName("code")]
+        public int Code { get; set; }
+        [JsonPropertyName("total")]
+        public int Total { get; set; } = 0;
+        [JsonPropertyName("data")]
+        public List<Book> Results { get; set; } = new List<Book>();
+    }
+}
